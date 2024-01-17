fc814a6ab826d7c9d31ea24b1faf9d1be8927d5f
e-b11 <96320276+e-b11@users.noreply.github.com>
2024-01-17 15:11:18 -0700

    programcompletion

--- Program.cs
+++ Program.cs
@@ -1,15 +1,57 @@
 ﻿using Mission2;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        //Create new instance of RollDice class
         RollDice rd = new RollDice();
         
+        //Intro
         System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
         System.Console.WriteLine("How many dice rolls would you like to simulate? ");
+        
+        //Get the number of rolls from user and assign it to an integer variable
         int numRolls = int.Parse(System.Console.ReadLine());
 
-        rd.RollResults(numRolls);
+        // Get the result of the RollResults method, should be an array of results
+        int[] rolls = rd.RollResults(numRolls);
+
+        //Result messages
+        System.Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");        System.Console.WriteLine("Total number of rolls = " + numRolls.ToString() + ".\n");
+
+        //Loop through the possible number results of rolling two dice
+        for (int num = 2; num < 13;  num++)
+        {
+            // Initialize variables
+            int numberOccurences = 0;
+            string output = "";
+
+            // Loop through the roll results in the array and keep track if the result matches the current number
+            for (int iCount = 0; iCount < rolls.Length; iCount++)
+            {
+                if (rolls[iCount] == num)
+                {
+                    numberOccurences += 1;
+                }
+            }
+
+            //Turn the number of occurences for the number into a percentage of the number of rolls, then round/convert it to an integer
+            double rollPercent = (double)numberOccurences / numRolls;
+            rollPercent *= 100;
+            int roundedPercent = (int)Math.Round(rollPercent);
+            
+            //Concatenate an output of the current number, and an asteric for each 1 percent of the number of rolls the current number was. Should look like a histogram at the end. 
+            output += ((num.ToString() + ": ") + new string('*', roundedPercent));
+            
+            //Show the output
+            System.Console.WriteLine(output);
+        }
+
+        //End message
+        System.Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
     }
 }
--- RollDice.cs
+++ RollDice.cs
@@ -8,32 +8,26 @@ namespace Mission2
 {
     internal class RollDice
     {
+        //Import Random class
         Random rnd = new Random();
-        public void RollResults(int num)
-        {
-            string output = "";
+        
+        //Method that will simulate the rolling of two dice, and putting the results in an array
+        public int[] RollResults(int num)
+        {          
+            //Create an array for the rolls, that is the length of the number of rolls the user requested
             int[] rolls = new int[num]; 
             
+            //Loop through and roll two dice, then add them together and put the result in an array
             for (int i = 0; i<num; i++)
             {
                 int Dice1 = rnd.Next(1, 7);
                 int Dice2 = rnd.Next(1, 7);
                 rolls[i] = Dice1 + Dice2;
             }
+
+            //Return an array of the roll results
+            return rolls;
             
-            for (int i = 2; i < 13; i++)
-            {
-                output += (i.ToString() + ": ");
-                for (int spn = 0; spn < rolls.Length; spn++)
-                {
-                    if (rolls[spn] == i)
-                    {
-                        output += "*";
-                    }
-                }
-                System.Console.WriteLine(output);
-                output = "";
-            }
         }
     }
 }
