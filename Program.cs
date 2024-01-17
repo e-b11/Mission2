734f71de165eb2e50bd988e1c374e41ce99ac737
e-b11 <96320276+e-b11@users.noreply.github.com>
2024-01-16 22:18:43 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        RollDice rd = new RollDice();
+        
+        System.Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        System.Console.WriteLine("How many dice rolls would you like to simulate? ");
+        int numRolls = int.Parse(System.Console.ReadLine());
+
+        rd.RollResults(numRolls);
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class RollDice
+    {
+        Random rnd = new Random();
+        public void RollResults(int num)
+        {
+            string output = "";
+            int[] rolls = new int[num]; 
+            
+            for (int i = 0; i<num; i++)
+            {
+                int Dice1 = rnd.Next(1, 7);
+                int Dice2 = rnd.Next(1, 7);
+                rolls[i] = Dice1 + Dice2;
+            }
+            
+            for (int i = 2; i < 13; i++)
+            {
+                output += (i.ToString() + ": ");
+                for (int spn = 0; spn < rolls.Length; spn++)
+                {
+                    if (rolls[spn] == i)
+                    {
+                        output += "*";
+                    }
+                }
+                System.Console.WriteLine(output);
+                output = "";
+            }
+        }
+    }
+}
